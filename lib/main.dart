e76e14fd0377d589e5252e9a2192b35f4363bf05
mandinu balasooriya <balasooriyamandinu@gmail.com>
2025-03-06 02:54:32 +0530

    done

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,5 @@
 import 'package:dr_cars/interface/servicerecords.dart';
 import 'package:flutter/material.dart';
-import 'interface/signin.dart';
-import 'interface/Service History.dart';
-import 'interface/profile.dart';
-import 'interface/Settings.dart';
 
 void main() {
   runApp(MyApp());
