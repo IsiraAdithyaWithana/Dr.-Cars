7b5d798ef0472943e83d8663be81e974dc91b64a
mandinu balasooriya <balasooriyamandinu@gmail.com>
2025-03-05 23:04:52 +0530

    ServiceHistorypage

--- lib/interface/Service History.dart
+++ lib/interface/Service History.dart
@@ -1,1 +1,21 @@
+import 'package:flutter/material.dart';
 
+class ServiceHistorypage extends StatelessWidget {
+  const ServiceHistorypage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Service History'),
+        backgroundColor: Colors.black,
+        leading: IconButton(
+          icon: Icon(Icons.arrow_back),
+          onPressed: () {
+            Navigator.pop(context); // Navigate back
+          },
+        ),
+      ),
+    );
+  }
+}
