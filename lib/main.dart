59a7eed0e5c11ab0dab2695854012509fe3f71c3
mandinu balasooriya <balasooriyamandinu@gmail.com>
2025-03-06 01:19:16 +0530

    done some changes

--- lib/main.dart
+++ lib/main.dart
@@ -12,6 +12,9 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(debugShowCheckedModeBanner: false, home: SignInScreen());
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: ServiceHistorypage(),
+    );
   }
 }
