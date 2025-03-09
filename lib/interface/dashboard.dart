89cb44b9ca9359c4c33dc069d76386a0e599906b
Sanuda de silva <sanudadesilva2003@gmail.com>
2025-03-09 21:01:32 +0530

    dash logo added

--- lib/interface/dashboard.dart
+++ lib/interface/dashboard.dart
@@ -28,96 +28,110 @@ class MyGarageScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(
-          'Your vehicle',
-          style: TextStyle(fontSize: 32, fontWeight: FontWeight.bold),
-        ),
-        backgroundColor: Colors.white,
-        elevation: 0,
-        iconTheme: IconThemeData(color: Colors.black),
-      ),
       body: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          SizedBox(height: 60),
-          Container(
-            height: 400,
-            child: ListView.builder(
-              scrollDirection: Axis.horizontal,
-              itemCount: cars.length,
-              itemBuilder: (context, index) {
-                final car = cars[index];
-                return Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 8.0),
-                  child: Container(
-                    width: 475,
-                    decoration: BoxDecoration(
-                      color: Colors.black,
-                      borderRadius: BorderRadius.circular(16),
-                      image: DecorationImage(
-                        image: AssetImage(car['image']),
-                        fit: BoxFit.cover,
-                        colorFilter: ColorFilter.mode(
-                          Colors.black.withOpacity(0.5),
-                          BlendMode.darken,
-                        ),
-                      ),
-                    ),
-                    child: Padding(
-                      padding: const EdgeInsets.all(12.0),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text(
-                            car['year'].toString(),
-                            style: TextStyle(color: Colors.white, fontSize: 22),
-                          ),
-                          Text(
-                            car['name'],
-                            style: TextStyle(
-                              color: Colors.white,
-                              fontSize: 42,
-                              fontWeight: FontWeight.bold,
-                            ),
-                          ),
-                          Spacer(),
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            children: [
-                              Text(
-                                '⚙️ ${car['km']} Kilometers',
-                                style: TextStyle(color: Colors.white),
-                              ),
-                              Text(
-                                '🛢 ${car['oil']} OIL',
-                                style: TextStyle(color: Colors.white),
-                              ),
-                            ],
-                          ),
-                        ],
-                      ),
-                    ),
-                  ),
-                );
-              },
+          SafeArea(
+            child: Padding(
+              padding: EdgeInsets.only(top: 10),
+              child: Image.asset('images/logo.png', height: 80),
             ),
           ),
-          SizedBox(height: 20),
           Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 16.0),
+            padding: const EdgeInsets.symmetric(vertical: 10),
             child: Text(
-              'Upcoming maintenance',
-              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+              'Your vehicle',
+              style: TextStyle(fontSize: 32, fontWeight: FontWeight.bold),
             ),
           ),
           Expanded(
-            child: ListView(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                ListTile(
-                  title: Text('2016 Focus RS'),
-                  subtitle: Text('5,301 miles'),
-                  trailing: Icon(Icons.check_circle, color: Colors.blue),
+                SizedBox(height: 10),
+                Container(
+                  height: 400,
+                  child: ListView.builder(
+                    scrollDirection: Axis.horizontal,
+                    itemCount: cars.length,
+                    itemBuilder: (context, index) {
+                      final car = cars[index];
+                      return Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                        child: Container(
+                          width: 475,
+                          decoration: BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.circular(16),
+                            image: DecorationImage(
+                              image: AssetImage(car['image']),
+                              fit: BoxFit.cover,
+                              colorFilter: ColorFilter.mode(
+                                Colors.black.withOpacity(0.5),
+                                BlendMode.darken,
+                              ),
+                            ),
+                          ),
+                          child: Padding(
+                            padding: const EdgeInsets.all(12.0),
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  car['year'].toString(),
+                                  style: TextStyle(
+                                    color: Colors.white,
+                                    fontSize: 22,
+                                  ),
+                                ),
+                                Text(
+                                  car['name'],
+                                  style: TextStyle(
+                                    color: Colors.white,
+                                    fontSize: 42,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                                Spacer(),
+                                Row(
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceBetween,
+                                  children: [
+                                    Text(
+                                      '⚙️ ${car['km']} Kilometers',
+                                      style: TextStyle(color: Colors.white),
+                                    ),
+                                    Text(
+                                      '🛢 ${car['oil']} OIL',
+                                      style: TextStyle(color: Colors.white),
+                                    ),
+                                  ],
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                      );
+                    },
+                  ),
+                ),
+                SizedBox(height: 20),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                  child: Text(
+                    'Upcoming maintenance',
+                    style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+                  ),
+                ),
+                Expanded(
+                  child: ListView(
+                    children: [
+                      ListTile(
+                        title: Text('2016 Corolla 141'),
+                        subtitle: Text('5,301 miles'),
+                        trailing: Icon(Icons.check_circle, color: Colors.blue),
+                      ),
+                    ],
+                  ),
                 ),
               ],
             ),
