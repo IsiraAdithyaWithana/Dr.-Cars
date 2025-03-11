8511db6290d601ce46bd4e3736d1e52aa2397a3d
nckodikara@students.nsbm.ac.lk <nckodikara@students.nsbm.ac.lk>
2025-03-11 15:15:16 +0530

    amendments

--- lib/main.dart
+++ lib/main.dart
@@ -23,6 +23,6 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(debugShowCheckedModeBanner: false, home: MainMenu());
+    return MaterialApp(debugShowCheckedModeBanner: false, home: AppointmentsPage());
   }
 }
