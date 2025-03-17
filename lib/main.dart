26eba5e191cc748df9a8779db5e27ccee7f748a8
Sanuda de silva <sanudadesilva2003@gmail.com>
2025-03-17 14:47:30 +0530

    save

--- lib/main.dart
+++ lib/main.dart
@@ -21,6 +21,9 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(debugShowCheckedModeBanner: false, home: Welcome());
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: DashboardScreen(),
+    );
   }
 }
