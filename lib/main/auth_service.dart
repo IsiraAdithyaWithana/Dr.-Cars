e0f823138a611c633a373dc595587d1ba7f3ca05
Isira Adithya Withana <issaadithyawithana@gmail.com>
2025-03-31 15:22:07 +0530

    keep current

--- lib/main/auth_service.dart
+++ lib/main/auth_service.dart
@@ -5,7 +5,6 @@ import 'package:google_sign_in/google_sign_in.dart';
 class AuthService {
   final FirebaseAuth _auth = FirebaseAuth.instance;
   final FirebaseFirestore _firestore = FirebaseFirestore.instance;
-  final GoogleSignIn _googleSignIn = GoogleSignIn();
 
   Future<User?> signInWithGoogle() async {
     try {
